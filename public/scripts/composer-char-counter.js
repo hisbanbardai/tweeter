dea34380d24d3b7ffb37605dc137183f24dd16f0
Hisban Shiraz <hisbanbardai@gmail.com>
2023-10-27 17:01:07 -0600

    Remove unnecessary comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -18,41 +18,5 @@ $(document).ready(function () {
         .children(".counter")
         .removeClass("color-counter");
     }
-
-    // if (charCount > charsLimit) {
-    //   $(this)
-    //     .siblings("#tweet-section")
-    //     .children(".counter")
-    //     .text(charsLeft)
-    //     .css("color", "red");
-    // } else {
-    //   $(this)
-    //     .siblings("#tweet-section")
-    //     .children(".counter")
-    //     .text(charsLeft)
-    //     .css("color", "#545149");
-    // }
   });
 });
-
-
-// document .ready () => {
-//   tweetText.on(input, function() {
-//     maxTweet= 140
-//     tweetLength = $this.val().length
-    
-    
-//     $sublin.children.text(maxLength -tweetLenght)
-  
-//     if(tweetLength > maxLength) {
-//       counter.addClass('colorCounter')
-      
-//     } else {
-//       counter.removeClass('coolorCounter')
-//     }
-    
-    
-//   })
-  
-  
-// }
